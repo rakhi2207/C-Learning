a8d1e20a615be39437f16376ff88a4fbab8677a7
Rakhi <jharakhi1211@gmail.com>
2021-07-19 23:02:48 +0530

    Learning

--- Tree/creatingBinaryTree.cpp
+++ Tree/creatingBinaryTree.cpp
@@ -56,14 +56,30 @@ void postorder(Node* root)
     }
 }
 
+    bool isBST(Node* root)
+    {
+        // code here.
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(root!=NULL&&root->val>root->left->val&&root->val<root->right->val)
+        {
+            cout<<root->val<<"\n";
+           isBST(root->left);
+            isBST(root->right);
+        }
+        return false;
+    }
+
 int main()
 {
     struct Node* root= new Node(4);
     root->left=new Node(3);
-    root->right=new Node(5);
-    root->left->left=new Node(6);
-    root->left->right=new Node(10);
-    root->right->left=new Node(16);
+    root->right=new Node(2);
+    root->left->left=new Node(1);
+    root->left->right=new Node(4);
+    root->right->left=new Node(3);
     root->right->right=new Node(7);
     cout<<"Preorder Traversal"<<"\n";
     preorder(root);
@@ -71,5 +87,6 @@ int main()
     inorder(root);
     cout<<"PostOrder Traversal"<<"\n";
     postorder(root);
+    cout<<isBST(root)<<"\n";
     return 0;
 }
